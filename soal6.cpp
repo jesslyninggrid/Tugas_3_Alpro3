4ed271a0eaa46ecd5aa07d56a1a7f5b3e93f4d99
haikalbuchari <haikalbuchari7@gmail.com>
2026-04-17 02:07:57 +0700

    Jawaban soal6.cpp

--- soal6.cpp
+++ soal6.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Masukkan bilangan : ";
+    cin >> n;
+
+    int total = 0;
+
+    // mulai dari 2, lompat 2 (biar cuma genap)
+    for (int i = 2; i <= n; i += 2) {
+        total += i * i; // tambahkan kuadratnya
+    }
+
+    cout << total;
+
+    return 0;
+}
