01c97fcd9d0a570a3ac9bdb374bd9dfdf0b89a92
haikalbuchari <haikalbuchari7@gmail.com>
2026-04-17 02:06:25 +0700

    Jawaban soal4.cpp

--- soal4.cpp
+++ soal4.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Masukkan n : ";
+    cin >> n; // input angka
+
+    cout << "H"; // cetak H dulu
+
+    // ulang huruf 'o' sebanyak n
+    for (int i = 0; i < n; i++) {
+        cout << "o";
+    }
+
+    cout << "r"; // cetak r
+
+    // ulang huruf 'e' sebanyak n
+    for (int i = 0; i < n; i++) {
+        cout << "e";
+    }
+
+    // ulang tanda seru sebanyak n
+    for (int i = 0; i < n; i++) {
+        cout << "!";
+    }
+
+    return 0;
+}
